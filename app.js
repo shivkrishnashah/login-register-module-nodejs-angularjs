4d8fede837cc508ebf7c407bafb932ffe661fdf6
shivkrishna shah <shahshivkrishna@hotmail.co.uk>
2019-06-12 00:45:12 +0530

    0.0.2
    - Added Validation in register form.
    - Added error handeling
    - Remove Email Duplicacy /User repeatation.[Duplicate email can not registered]

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 8000
 var path = require("path");
 var router = express.Router();
 var bodyParser = require('body-parser');
@@ -19,7 +19,8 @@ var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
   password : '',
-  database : 'notes'
+  database : 'notes',
+
 });
 connection.connect();
 
@@ -27,22 +28,34 @@ app.use('/', express.static(path.join(__dirname, 'login')));
 
 app.use('/saveData', function (req, res, next) {
 
-  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-    // Store hash in your password DB.
-    console.log('hash', hash);
-    let query = "INSERT INTO `users`(`password`, `name`, `contact`, `email`, `created_on`, `modified_on`) VALUES (?,?,?,?,?,?)";
-    let data =[hash, req.body.username,  req.body.contact, req.body.email, +new Date(), +new Date()]
-    connection.query(query, data, function (error, results) {
-      if (error) throw error;
-      let data = {
-        status: true
-      }
-      res.jsonp(data);
-    });
+  let check_email = "SELECT * FROM users WHERE email = "+connection.escape(req.body.email);
+  console.log(check_email);
+  connection.query(check_email, function (error, results) {
+    if(error){
+      let response = {status:false, message: "Error in adding."};
+      res.jsonp(response);
+    }else{
+        if(results.length>0){
+          let response = {status:false, message: "Email Already Exists."};
+          res.jsonp(response);
+        }else{
+          bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+            // Store hash in your password DB.
+            let query = "INSERT INTO `users`(`password`, `name`, `contact`, `email`, `created_on`, `modified_on`) VALUES (?,?,?,?,?,?)";
+            let data =[hash, req.body.username,  req.body.contact, req.body.email, +new Date(), +new Date()]
+            connection.query(query, data, function (error, results) {
+                if (error) {
+                  let response = {status:false, message: "Error in adding."};
+                  res.jsonp(response);
+                }else{
+                  let response  = {status:true, message: "Added Successfully."};
+                  res.jsonp(response);
+                }
+            });
+          });
+        }
+    }
   });
-
-
-
 }, function (req, res, next) {
   console.log('Request Type:', req.method)
 })
--- login/controllers/loginController.js
+++ login/controllers/loginController.js
@@ -1,5 +1,10 @@
-app.controller('loginController', function($scope, $http  ){
+/*
+ Author - Shivkrishna Shah
+ Date - 11-06-2019
+*/
+app.controller('loginController', function($scope, $http, $timeout  ){
 
+// Desc- Initializtion
   $scope.init = function(){
     $scope.user = {
       username : "",
@@ -9,16 +14,33 @@ app.controller('loginController', function($scope, $http  ){
     }
   }
 
-
-  $scope.login = function(){
-
+// Desc - Common Methods For SHow Errors
+  function showSuccessMessage(message){
+    $scope.successMesssage =message;
+    $timeout(function(){
+      $scope.successMesssage = "";
+    },5000);
+  }
+  function showErrorMessage(message){
+    $scope.errorMesssage =message;
+    $timeout(function(){
+      $scope.errorMesssage = "";
+    },5000);
   }
 
-  $scope.register = function(){
-    $scope.signupForm.$setPristine();
-    // $http.post('http://192.168.43.43:3000/saveData', $scope.user).success(function(res, req) {
-    //     console.log('res',res );
-    // });
+// Desc - For Register User
+  $scope.register = function(valid){
+    if(valid){
+      $http.post('http://192.168.43.43:8000/saveData', $scope.user).success(function(res, req) {
+        if(res.status == false){
+             showErrorMessage(res.message);
+        }else{
+             showSuccessMessage(res.message);
+             $scope.user = {}
+             $scope.signupForm.$setPristine();
+        }
+      });
+    }
   }
 
 })
